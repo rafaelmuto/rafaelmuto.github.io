2f78a39cc3b5127518b4a0aa4789f9cc1628a1f8
Rafael Muto <r.nagahama@gmail.com>
2018-12-19 18:24:14 -0200

    scroll problens solved

--- JS/console.js
+++ JS/console.js
@@ -14,11 +14,19 @@ window.onload = function() {
       print_line(cmd);
       cmd_line.value = '';
       loader(cmd);
+      // window.scrollTo(0, document.body.clientHeight);
+      window.scrollTo(0, document.body.scrollHeight);
     }
   });
 
   function loader(string) {
     switch (cmd) {
+      case 'fill':
+        for (let i = 0; i < 100; i++) {
+          print_line(i);
+          print_line('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+        }
+        break;
       case 'help':
         print_line('=================');
         print_line('List of Commands:');
