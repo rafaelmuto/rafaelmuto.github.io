ebb6988f3dd47b66582f8ff4dc1a0bacf5ed6325
rafaelmuto <r.nagahama@gmail.com>
2020-06-17 22:39:28 -0300

    organization

--- JS/console.js
+++ JS/console.js
@@ -1,23 +1,23 @@
 window.onload = () => {
-  const input = document.getElementById("input");
-  const cmd_line = document.getElementById("cmd_line");
-  const print_out = document.getElementById("print");
+  const input = document.getElementById('input');
+  const cmd_line = document.getElementById('cmd_line');
+  const print_out = document.getElementById('print');
 
   let ln = 0;
 
   const lorem =
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-  document.addEventListener("keydown", () => {
+  document.addEventListener('keydown', () => {
     cmd_line.focus();
   });
 
-  cmd_line.addEventListener("keydown", e => {
-    if (e.key == "Enter") {
-      input.style.display = "none";
+  cmd_line.addEventListener('keydown', e => {
+    if (e.key == 'Enter') {
+      input.style.display = 'none';
       cmd = cmd_line.value;
       print_line(cmd);
-      cmd_line.value = "";
+      cmd_line.value = '';
       cmdLoader(cmd);
       window.scrollTo(0, document.body.scrollHeight);
     }
@@ -26,10 +26,10 @@ window.onload = () => {
   // print entered cmd:
   const print_line = string => {
     ln++;
-    let line = document.createTextNode(">>" + string);
-    let new_line = document.createElement("p");
+    let line = document.createTextNode('>>' + string);
+    let new_line = document.createElement('p');
     new_line.appendChild(line);
-    new_line.id = "LN" + ln;
+    new_line.id = 'LN' + ln;
     print_out.appendChild(new_line);
   };
 
@@ -39,9 +39,9 @@ window.onload = () => {
     ln++;
     let i = 0;
 
-    let new_line = document.createElement("p");
-    print_out.appendChild(new_line).id = "LN" + ln;
-    new_line = document.getElementById("LN" + ln);
+    let new_line = document.createElement('p');
+    print_out.appendChild(new_line).id = 'LN' + ln;
+    new_line = document.getElementById('LN' + ln);
 
     const typer = new Promise(resolve => {
       let timer = setInterval(() => {
@@ -49,86 +49,86 @@ window.onload = () => {
         i++;
         if (i > string.length) {
           clearInterval(timer);
-          resolve("typer resolved");
+          resolve('typer resolved');
         }
       }, speed);
     });
 
     typer.then(res => {
       // console.log(res);
-      input.style.display = "grid";
+      input.style.display = 'grid';
     });
   };
 
   // cmdLoader List:
   const cmdLoader = string => {
     switch (string) {
-      case "lorem":
+      case 'lorem':
         printer(lorem);
         break;
 
-      case "help":
-        printer("List of Commands:");
-        printer("CV => CURRICULUN VITAE rafaelmuto");
-        printer("CON => contact info");
-        printer("TIME for standard UNIX time.");
-        printer("CANNON => jogo do canhão");
-        printer("CLS or CLEAR to clear screen.");
-        printer("RESET or RELOAD to reload the page.");
+      case 'help':
+        printer('List of Commands:');
+        printer('CV => CURRICULUN VITAE rafaelmuto');
+        printer('CON => contact info');
+        printer('TIME for standard UNIX time.');
+        printer('CANNON => jogo do canhão');
+        printer('CLS or CLEAR to clear screen.');
+        printer('RESET or RELOAD to reload the page.');
         break;
 
-      case "con":
-        printer("email: r.nagahama@gmail.com");
-        printer("mobile: +55 11 947 256 697");
-        printer("GitHub: https://github.com/rafaelmuto");
-        printer("portifolio: https://cargocollective.com/rafaelmuto");
+      case 'con':
+        printer('email: r.nagahama@gmail.com');
+        printer('mobile: +55 11 947 256 697');
+        printer('GitHub: https://github.com/rafaelmuto');
+        printer('portifolio: https://cargocollective.com/rafaelmuto');
         break;
 
-      case "clear":
-        print_out.innerHTML = "";
-        input.style.display = "grid";
+      case 'clear':
+        print_out.innerHTML = '';
+        input.style.display = 'grid';
         break;
 
-      case "cls":
-        print_out.innerHTML = "";
-        input.style.display = "grid";
+      case 'cls':
+        print_out.innerHTML = '';
+        input.style.display = 'grid';
         break;
 
-      case "time":
+      case 'time':
         let d = new Date();
         let time = d.getTime();
-        printer("Its been " + time + "ms since midnight January 1, 1970.");
+        printer('Its been ' + time + 'ms since midnight January 1, 1970.');
         break;
 
-      case "reset":
+      case 'reset':
         location.reload();
         break;
 
-      case "reload":
+      case 'reload':
         location.reload();
         break;
 
-      case "ln":
+      case 'ln':
         let lnp = ln + 1;
-        printer("LN" + lnp);
+        printer('LN' + lnp);
         break;
 
-      case "cannon":
-        printer("game...");
-        location.assign("cannon/index.html");
+      case 'cannon':
+        printer('game...');
+        location.assign('cannon/index.html');
         break;
 
-      case "ring":
-        printer("this is a colorfull ring...");
-        location.assign("ring.html");
+      case 'ring':
+        printer('this is a colorfull ring...');
+        location.assign('HTML/ring.html');
         break;
 
-      case "cv":
-        location.assign("Files/rafaelmuto_CV.pdf");
+      case 'cv':
+        location.assign('Files/rafaelmuto_CV.pdf');
         break;
 
       default:
-        printer("ERROR: " + cmd + " is not a command.");
+        printer('ERROR: ' + cmd + ' is not a command.');
     }
   };
 };
