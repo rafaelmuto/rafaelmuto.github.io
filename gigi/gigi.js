00a7d3f776d9fb95c71035bc2a4b92aed283ffb0
rafaelmuto <r.nagahama@gmail.com>
2022-02-16 11:29:22 -0300

    gigi update

--- gigi/gigi.js
+++ gigi/gigi.js
@@ -1,9 +1,14 @@
-console.log('gigi.js')
+console.log('>> gigi.js')
 
-let today = new Date()
-let bDay = new Date('05/10/2021')
+const _MS_PER_DAY = 1000 * 60 * 60 * 24
 
-const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+const today = new Date()
+const b_date = new Date('05/10/1991')
+const first_date = new Date('11/21/2020')
+const first_ily_date = new Date('01/01/2021')
+const ani_date = new Date('02/26/2021')
+
 
 function dateDiffInDays(a, b) {
     const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate())
@@ -12,6 +17,21 @@ function dateDiffInDays(a, b) {
     return Math.floor((utc2 - utc1) / _MS_PER_DAY)
 }
 
-let counter = dateDiffInDays(today, bDay)
+function dateTodayDiffThisYearInDays(date) {
+    const this_year = new Date()
 
-document.getElementById('days').innerHTML = counter
+    const utc1 = Date.UTC(this_year.getFullYear(), this_year.getMonth(), this_year.getDate())
+    const utc2 = Date.UTC(this_year.getFullYear(), date.getMonth(), date.getDate())
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY)
+}
+
+document.getElementById('b_day').innerHTML = dateTodayDiffThisYearInDays(b_date)
+document.getElementById('text').innerHTML =
+    'Há ' +
+    dateDiffInDays(first_date, today) +
+    ' dias atras nos achamos nesse mundo. E nos declaramos pela primeira vez a ' +
+    dateDiffInDays(first_ily_date, today) +
+    ' E já fazem ' +
+    dateDiffInDays(ani_date, today) +
+    ' dias que dormimos juntinhos...'
