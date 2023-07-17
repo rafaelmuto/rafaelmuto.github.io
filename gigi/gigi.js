bf5eb2f237282895f10a383964f82f4e44dd3f8a
Rafael Muto <r.nagahama@gmail.com>
2023-07-17 20:18:33 -0300

    get next happy day

--- gigi/gigi.js
+++ gigi/gigi.js
@@ -1,10 +1,7 @@
 console.log('>> gigi.js')
 
-const _MS_PER_DAY = 1000 * 60 * 60 * 24
-
-
 const today = new Date()
-const b_date = new Date('05/10/1991')
+// 10 - 05 - 1991
 const first_date = new Date('11/21/2020')
 const first_ily_date = new Date('01/01/2021')
 const ani_date = new Date('02/26/2021')
@@ -13,35 +10,49 @@ const ani_date = new Date('02/26/2021')
 function getDaysDifference(startDate, endDate) {
     // Calculate the time difference in milliseconds
     const timeDiff = endDate.getTime() - startDate.getTime();
-  
+
     // Convert milliseconds to days
     const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-  
+
     // Factor in leap years
     const leapYears = countLeapYears(startDate, endDate);
     const adjustedDaysDiff = daysDiff - leapYears;
-  
+
     return adjustedDaysDiff;
-  }
-  
-  function countLeapYears(startDate, endDate) {
+}
+
+function countLeapYears(startDate, endDate) {
     let count = 0;
     for (let year = startDate.getFullYear(); year <= endDate.getFullYear(); year++) {
-      if (isLeapYear(year)) {
-        count++;
-      }
-    }
-    return count;
-  }
-  
-  function isLeapYear(year) {
+            if (isLeapYear(year)) {
+                count++;
+            }
+}
+return count;
+}
+
+function isLeapYear(year) {
     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-  }
+}
+
+
+function getNextGigiHappyDay()
+{
+    let d = new Date()
+    let year = d.getFullYear()
+    let month = d.getMonth() + 1
+    let day = d.getDate()
+
+    if(month > 5  && day > 10) {
+        year++
+    } 
+
+    return new Date(year + '/' + month + '/' + day + 1)
+}
 
 
 
-
-document.getElementById('b_day').innerHTML = getDaysDifference(b_date, today) + 1
+document.getElementById('b_day').innerHTML = getDaysDifference(getNextGigiHappyDay(), today)
 
 document.getElementById('text').innerHTML =
     'Há ' +
